f348860626e986937558ea6f73140800ad29e1af
Dmytro <kobylynskiy.d@outlook.com>
2017-02-17 15:09:36 +0200

    33

--- Angular2App/Controllers/HomeController.cs
+++ Angular2App/Controllers/HomeController.cs
@@ -10,51 +10,14 @@ namespace Angular2App.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet("[action]/{city}")]
-        public async Task<IActionResult> City(string city)
+        public IActionResult Index()
         {
-            using (var client = new HttpClient())
-            {
-                try
-                {
-                    client.BaseAddress = new Uri("http://api.openweathermap.org");
-                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&&appid=8c18f6ec9fe6170c8f31b63f4c2c59c4&units=metric");
-                    response.EnsureSuccessStatusCode();
-
-                    var stringResult = await response.Content.ReadAsStringAsync();
-                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
-                    return Ok(new
-                    {
-                        Temp = rawWeather.Main.Temp,
-                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
-                        City = rawWeather.Name
-                    });
-                }
-                catch (HttpRequestException httpRequestException)
-                {
-                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
-                }
-            }
+            return View();
         }
-    }
 
-    public class OpenWeatherResponse
-    {
-        public string Name { get; set; }
-
-        public IEnumerable<WeatherDescription> Weather { get; set; }
-
-        public Main Main { get; set; }
-    }
-
-    public class WeatherDescription
-    {
-        public string Main { get; set; }
-        public string Description { get; set; }
-    }
-
-    public class Main
-    {
-        public string Temp { get; set; }
+        public IActionResult Error()
+        {
+            return View();
+        }
     }
 }
