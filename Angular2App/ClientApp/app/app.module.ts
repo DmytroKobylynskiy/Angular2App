c09642faa7436e841292e1ab2018ee34afb18b4e
Dmytro <kobylynskiy.d@outlook.com>
2017-02-17 15:07:35 +0200

    22

--- Angular2App/ClientApp/app/app.module.ts
+++ Angular2App/ClientApp/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { UniversalModule } from 'angular2-universal';
+
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { HelloWorldComponent } from './components/helloworld/helloworld.component';
 import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
@@ -17,15 +19,18 @@ import { WeatherComponent } from './components/weather/weather.component';
         CounterComponent,
         FetchDataComponent,
         HomeComponent,
+        HelloWorldComponent,
         WeatherComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'hello', component: HelloWorldComponent },
             { path: 'weather', component: WeatherComponent },
             { path: '**', redirectTo: 'home' }
         ])
